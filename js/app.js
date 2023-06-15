fd379c609d7495d7937eb94a96d27e9675400d75
homeristico <homeristico@hotmail.com>
2023-06-15 16:37:08 -0500

    calculo dia juliano y año decimales

--- js/app.js
+++ js/app.js
@@ -9,9 +9,34 @@
 // })();
 
 
+
+function calcularDiaDelAno(fecha) {
+  var inicioAño = new Date(fecha.getFullYear(), 0, 0);  
+  var tiempoTranscurrido = fecha - inicioAño;  
+  
+  var milisegundosEnUnDía = 24 * 60 * 60 * 1000;
+
+  var diaDelAño = Math.floor(tiempoTranscurrido / milisegundosEnUnDía);
+  return diaDelAño;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.querySelector('#cargarTexto').addEventListener('change', (e) => {
     
-    let file = e.target.files[0];    
+    let file = e.target.files[0];
     //console.log(file);    
 
 
@@ -29,12 +54,41 @@ document.querySelector('#cargarTexto').addEventListener('change', (e) => {
 document.querySelector('#cargarHtml').addEventListener('change', (e) => {
     
     let file = e.target.files[0];    
-    //console.log(file);    
-
+    //console.log(file);   
 
     let reader = new FileReader();
     reader.onload = (e) => {                
-        console.log(e.target.result.split("<tr>"));
+
+
+        // arreglo con <tr>
+        //console.log(e.target.result.split("<tr>"));
+        for(let element of e.target.result.split("</tr>")){
+          
+          if(element.indexOf("Intervalo de observación:") !== -1){
+            // console.log(element)
+            // console.log(element.substring(85, 96));
+
+            
+            
+            const fecha = element.substring(85, 96);
+            const ano = fecha.substring(6,10);
+            const mes = fecha.substring(3,5);
+            const dia = fecha.substring(0,2);
+            console.log(ano, mes, dia);
+            
+
+            // Ejemplo de uso:
+            var fechaEjemplo = new Date(ano, mes-1, dia);
+            var diaDelAno = calcularDiaDelAno(fechaEjemplo);
+            console.log(ano)
+            let anoEpoca = parseInt(ano) + (diaDelAno/365);
+            console.log(diaDelAno, anoEpoca);
+            
+            
+            break;
+          }
+        }        
+        
     };
     reader.readAsText(file);
     
@@ -47,7 +101,9 @@ document.getElementById("cargarCarpeta").addEventListener("change",function(ev){
   //console.log(ev.target.files);
 
   for(let i of ev.target.files){
-    console.log(i);
+    if(i.name.indexOf("html") !== -1){
+      console.log(i.name);
+    }
   }
 //   for (i = 0; i < ev.target.files.length; i++) {
 //       let file = ev.files[i];
